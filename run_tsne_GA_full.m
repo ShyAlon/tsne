ed4c49a632e3632b030304ee5ebc10bbd0d853aa
Shy Alon <shy.alon@gmail.com>
2017-08-12 13:50:12 +0300

    fun fun

--- run_tsne_GA_full.m
+++ run_tsne_GA_full.m
@@ -2,7 +2,8 @@
 %% run the experiment 100 times
 for experimentCount = 1:10
     % clear('all')
-    for trial = 6:6
+    for trial = 3:8
+        population_size = 20;
         clusterGen = 128;
         % filename = websave('mnist_train.mat', 'https://github.com/awni/cs224n-pa4/blob/master/Simple_tSNE/mnist_train.mat?raw=true');
         if trial == 0
@@ -19,7 +20,7 @@ for experimentCount = 1:10
         elseif trial == 3  
         %% CMC Database
             local = './large_files/CMC_DataBase.csv';
-            tags = './large_files/CMC_DataBase_labels.csv';
+            tags = '../large_files/CMC_DataBase_labels.csv';
             name = 'CMC';
         elseif trial == 4  
         %% Bitter Database
@@ -34,6 +35,7 @@ for experimentCount = 1:10
             local = './large_files/logbbb.csv';
             name = 'LogBBB';
             clusterGen = 256;
+            population_size = 40;
         elseif trial == 7  
         %% FeGaPd Database
             local = './large_files/FeGaPd_Exp_PP.csv';
@@ -49,16 +51,20 @@ for experimentCount = 1:10
 
         %% Read the data source
 
-        new = csvread(local);
-        if trial == 3 || trial == 8
-            data = new(:, 1:end);
-            fid = fopen(tags,'r');
-            labels = textscan(fid, '%q');
-            labels = cellstr( labels{1});
-            fclose(fid);  
-        else
-            data = new(:, 1:end-1);
-            labels = new(:, end);
+        try
+            new = csvread(local);
+            if trial == 3 || trial == 8
+                data = new(:, 1:end);
+                fid = fopen(tags,'r');
+                labels = textscan(fid, '%q');
+                labels = cellstr( labels{1});
+                fclose(fid);  
+            else
+                data = new(:, 1:end-1);
+                labels = new(:, end);
+            end
+        catch ME
+            continue
         end
 
         y = unique(labels);
@@ -84,8 +90,8 @@ for experimentCount = 1:10
         end
 
 
-        [m,n] = size(data)
-        % labels = labels';
+        [m,n] = size(data);
+        % labels = labels';e z
 
         %% Process the data
         h1 = figure;
@@ -104,7 +110,6 @@ for experimentCount = 1:10
         iMap = 6;
         iFeatures = 7;
         population = [];
-        population_size = 20;
         maps = [];
         features = [];
         populationFeatures = {};
@@ -172,30 +177,27 @@ for experimentCount = 1:10
                     top_q3 = population{1, iQuality3};
                     top_q5 = population{1, iQuality5};
                     top_trust = population{1, iTrust};
-                    a = [zeros(1,4-mod(numel(population{sz})-1,4)-1) population{sz}];
-                    hexFeatures = dec2hex(bin2dec(num2str(reshape(a,4,[])','%1d')))'
-                    top_features = hexFeatures % binaryVectorToHex(population{sz});
+                    top_features = binaryVectorToHex(population{sz});
                     map = inter_map;
-                    h = gscatter(inter_map(:,1), inter_map(:,2), labels);
-                    % scatter3(inter_map(:,1), inter_map(:,2), inter_map(:,2),3,labels)
-                    % view(40,35)
-                    % csvwrite
-                           
+                    gscatter(inter_map(:,1), inter_map(:,2), labels);
+
                     if trial == 3 || trial == 8
                         modeText = mode{1};
                     else
                         modeText = sprintf('%d', mode(1));
                     end
-                    %message = sprintf('../results/%s_%s_population_%0.3f.xls',name, modeText, generations)
-                    %xlswrite(message, population);
-                    filename = sprintf('../results/%s_%s_q_%0.3f_q3_%0.3f_q5_%0.3f_trust_%0.3f_features_%s.png',name, modeText, top_q, top_q3, top_q5, top_trust, top_features);
-                    title(sprintf('%s %s q %0.3f q3 %0.3f q5 %0.3f trust %0.3f features %s.png',name, modeText, top_q, top_q3, top_q5, top_trust, top_features));
+                    legend(modeText, sprintf('Not %s', modeText));
+                    
+                    message = sprintf('results/%s_%s_population_%0.3f.xls',name, modeText, generations);
+                    xlswrite(message, population);
+                    filename = sprintf('results/%s_%s_q_%0.3f_q3_%0.3f_q5_%0.3f_trust_%0.3f_features_%s.png',name, modeText, top_q, top_q3, top_q5, top_trust, top_features);
+                    title(sprintf('%s %s q %0.3f',name, modeText, top_q), modeText, sprintf('Not %s', modeText));
                     saveas(h1, filename);
-                    figfilename = sprintf('../results/%s_%s_q_%0.3f_q3_%0.3f_q5_%0.3f_trust_%0.3f_features_%s.fig',name, modeText, top_q, top_q3, top_q5, top_trust, top_features);
+                    figfilename = sprintf('results/%s_%s_q_%0.3f_q3_%0.3f_q5_%0.3f_trust_%0.3f_features_%s.fig',name, modeText, top_q, top_q3, top_q5, top_trust, top_features);
                     savefig(h1,figfilename);
-                    %xlswrite(sprintf('../results/%s_%s_q_%0.3f_q3_%0.3f_q5_%0.3f_trust_%0.3f__features_%s.xls',name, modeText, top_q, top_q3, top_q5, top_trust, top_features), map);
+                    xlswrite(sprintf('results/%s_%s_q_%0.3f_q3_%0.3f_q5_%0.3f_trust_%0.3f__features_%s.xls',name, modeText, top_q, top_q3, top_q5, top_trust, top_features), map);
                     allKeys = keys(top_classes);
-                    csvClasses = []
+                    csvClasses = [];
                     row = 1;
                     for i = 1:size(allKeys, 2)
                         csvClasses(1, row) = i; %allKeys{i};
@@ -205,7 +207,7 @@ for experimentCount = 1:10
                         end
                         row = row + 1;
                     end
-                    csvwrite(sprintf('../results/%s_q_%0.3f_classes.csv',name, top_q), csvClasses);
+                    csvwrite(sprintf('results/%s_q_%0.3f_classes.csv',name, top_q), csvClasses);
                 catch ME
                     message = 'Failed writing the result files'
                 end 
